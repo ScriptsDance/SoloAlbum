8396096876588ef01219864c18b6de7acf0f2afb
Kevin <>
2022-05-09 10:16:50 -0700

    Pull CreatePlaylist feature from dev

--- client/components/SpotifyList.jsx
+++ client/components/SpotifyList.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import SpotifyTrack from './SpotifyTrack';
+// import SpotifyTrack from './SpotifyTrack';
 
 class SpotifyList extends Component {
   render() {
+    // Spotify API responds JSON to GET request
+    // song represents passed
+    const songs = [];
+    for (let i = 0; i < songs.length; i++) {
+      songs.push(
+        <span>
+          <label>${songName}</label>
+          <label>${artist}</label>
+        </span>
+      )
+    }
     return (
       <div>
-        
+        [songs]
       </div>
     )
   }
