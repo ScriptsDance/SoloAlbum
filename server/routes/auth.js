b072593c949919a2f4f037f8ee4d981749cc20f9
Yang Song <yangsong531@gmail.com>
2022-05-09 17:46:25 -0700

    before dinner

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,37 @@
-const express = require('express');
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+const express = require ('express');
 const router = express.Router();
 
-const client_id = '25cd22c9cb434ee8b778937b518d139b';
-const redirect_uri = 'http://localhost:3000/callback/';
+
+var client_id = '25cd22c9cb434ee8b778937b518d139b'; // Your client id
+var client_secret = '8ed1549c6537407ab0d792ef6fd56511'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
 
 router.get('/login', function(req, res) {
+  console.log("i am in the login call");
+
   var state = generateRandomString(16);
+  res.cookie(stateKey, state); //set {'stateKey': state} to response cookie
+
+  // your application requests authorization
   var scope = 'user-read-private user-read-email';
- res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
+  // res.redirect('https://accounts.spotify.com/authorize?' +
+  res.redirect('https://accounts.spotify.com/authorize?'+
+  new URLSearchParams({
       response_type: 'code',
       client_id: client_id,
       scope: scope,
@@ -18,16 +41,22 @@ router.get('/login', function(req, res) {
 });
 
 router.get('/callback', function(req, res) {
-
+  console.log("i am in the callback call");
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+  console.log('req.query:', req.query);
+  console.log('storedstate: ',storedState);
   var code = req.query.code || null;
   var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  if (state === null) {
+  if (state === null || state !== storedState) {
     res.redirect('/#' +
-      querystring.stringify({
+    new URLSearchParams({
         error: 'state_mismatch'
       }));
   } else {
+    res.clearCookie(stateKey);
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
@@ -36,15 +65,47 @@ router.get('/callback', function(req, res) {
         grant_type: 'authorization_code'
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        'Authorization': 'Basic ' + (new URLSearchParams(client_id + ':' + client_secret).toString('base64'))
       },
       json: true
     };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+        new URLSearchParams({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+        new URLSearchParams({
+            error: 'invalid_token'
+          }));
+      }
+    });
   }
 });
 
 router.get('/refresh_token', function(req, res) {
 
+  // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -65,7 +126,6 @@ router.get('/refresh_token', function(req, res) {
     }
   });
 });
-
 
 
 
